a69eaed20b0129a12fbc09a5ea46478491ef432c
Jason <150493510+Behindtheboard@users.noreply.github.com>
2024-03-14 00:31:20 -0700

    Create displayInput function for readability

--- script.js
+++ script.js
@@ -23,22 +23,26 @@ let result;
 const operate = function(firstNumInput, secNumInput, operator) {
     switch (operator) {
         case '/':
-            display.textContent = `${divide(firstNumInput, secNumInput)}`;
+            result = divide(firstNumInput, secNumInput);
+            display.textContent = `${result}`;
             console.log(`result is ${divide(firstNumInput, secNumInput)}`);
         break;
 
         case 'x':
-            display.textContent = `${multiply(firstNumInput, secNumInput)}`;
+            result = multiply(firstNumInput, secNumInput);
+            display.textContent = `${result}`;
             console.log(`result is ${multiply(firstNumInput, secNumInput)}`);
         break;
 
         case '-':
-            display.textContent = `${minus(firstNumInput, secNumInput)}`;
+            result = minus(firstNumInput, secNumInput);
+            display.textContent = `${result}`;
             console.log(`result is ${minus(firstNumInput, secNumInput)}`);
         break;
         
         case '+':
-            display.textContent = `${add(firstNumInput, secNumInput)}`;
+            result = add(firstNumInput, secNumInput);
+            display.textContent = `${result}`;
             console.log(`result is ${add(firstNumInput, secNumInput)}`);
         break;
     }
@@ -47,11 +51,9 @@ const operate = function(firstNumInput, secNumInput, operator) {
 const display = document.querySelector('#display');
 const buttons = document.querySelector('#calculator');
 
-const screenArr = [];
 const numArr = [];
 
 const clearArrays = function() {
-    screenArr.splice(0, screenArr.length);
     numArr.splice(0, numArr.length);
 }
 
@@ -60,14 +62,18 @@ const combineInputs = function(num) {
     return numArr.join('');
 }
 
+const displayInput = function(input){
+    display.textContent = `${input}`;
+}
+
 const numInputs = function(num) {
     if (operator === undefined) {
         firstNumInput = Number(combineInputs(num));
-        display.textContent = `${firstNumInput}`;
+        displayInput(firstNumInput);
         console.log(`first is ${firstNumInput}`);
     } else {
         secNumInput = Number(combineInputs(num));
-        display.textContent = `${secNumInput}`;
+        displayInput(secNumInput);
         console.log(`sec is ${secNumInput}`);
     }    
 }
@@ -114,6 +120,27 @@ buttons.addEventListener('click', (event) => {
             secNumInput = undefined;
         break;
         case 'plusMinus':
+            if (operator === undefined) {
+                if (firstNumInput > 0) {
+                    firstNumInput = -firstNumInput;
+                    displayInput(firstNumInput);
+                } else if (firstNumInput < 0) {
+                    firstNumInput = Number(firstNumInput
+                        .toString()
+                        .slice(1,firstNumInput.length));
+                    displayInput(firstNumInput);
+                }
+            } else {
+                if (secNumInput > 0) {
+                    secNumInput = -secNumInput;
+                    displayInput(secNumInput);
+                } else if (secNumInput < 0) {
+                    secNumInput = Number(secNumInput
+                        .toString()
+                        .slice(1,secNumInput.length));
+                    displayInput(secNumInput);
+                }
+            }    
         break;
         case 'percentage':
         break;
